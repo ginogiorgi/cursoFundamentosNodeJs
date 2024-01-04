2bb88b7bd90712538a795896e99e0c75fafdb793
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-01-04 02:00:04 -0300

    Clase 5 Curso de Fundamentos de Node.js

---
+++ src/monohilo.js
@@ -0,0 +1,11 @@
+let i = 0;
+
+console.log("Primera instruccción");
+setInterval(() => {
+  console.log(i);
+  i++;
+  //   if (i === 5) {
+  //     let a = 3 + z;
+  //   }
+}, 1000);
+console.log("Segunda instruccción");
