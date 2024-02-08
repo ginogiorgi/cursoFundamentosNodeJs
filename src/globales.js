30e4f9399773a9d2fe8c526ab6e62cab4af023c8
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-07 22:50:22 -0300

    Clase 11 Curso de Fundamentos de Node.js

---
+++ src/globales.js
@@ -0,0 +1,22 @@
+// let i = 0;
+// let intervalo = setInterval(() => {
+//   console.log("Hola");
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+// }, 1000);
+
+// setImmediate(() => {
+//   console.log("Hola");
+// });
+
+// console.log(process);
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+global.miVariable = "Hola";
+
+console.log(miVariable);
