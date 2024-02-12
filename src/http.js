bbeef57f257757707dfca6884ec835413f5cd58c
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-12 03:30:50 -0300

    Clase 17 Curso de Fundamentos de Node.js

---
+++ src/http.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+function router(request, response) {
+  console.log("Nueva petición");
+  console.log(request.url);
+  switch (request.url) {
+    case "/hola":
+      response.write("Hola, que tal");
+      response.end();
+      break;
+    default:
+      response.write("Error 404: No se lo que quieres");
+      response.end;
+  }
+
+  //   response.writeHead(201, { "Content-Type": "text/plain" });
+  //   response.write("Hola, ya se usar http de NodeJS");
+  //   response.end();
+}
+
+http.createServer(router).listen(3000);
+
+console.log("Escuchando http en el puerto 3000");
